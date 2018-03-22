7bfeb4aa86942067bd9bc71145f6b5fac347f0a4
forhadparvez <forhad.parvez@yahoo.com>
2018-03-22 15:00:51 +0600

    Create Data Models And PatientRepository

---
+++ QueueManagement.Repository/PatientSection/PatientRepository.cs
@@ -0,0 +1,61 @@
+﻿
+using QueueManagement.Repository.DataModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueueManagement.Repository.PatientSection
+{
+    public class PatientRepository
+    {
+        private readonly QueueManagementDbEntities _dbEntities;
+
+        public PatientRepository()
+        {
+            _dbEntities = new QueueManagementDbEntities();
+        }
+
+        public int Save(Patient patient)
+        {
+            _dbEntities.Patients.Add(patient);
+            return _dbEntities.SaveChanges();
+        }
+
+        public int Update(int id, Patient patient)
+        {
+            var patientInDb = _dbEntities.Patients.SingleOrDefault(c => c.Id == id);
+            if (patientInDb != null)
+            {
+                patientInDb.Name = patient.Name;
+                patientInDb.PhoneNo = patient.PhoneNo;
+                patientInDb.Age = patient.Age;
+
+                return _dbEntities.SaveChanges();
+            }
+
+            return 0;
+        }
+
+        public int Delete(int id)
+        {
+            var patientInDb = _dbEntities.Patients.SingleOrDefault(c => c.Id == id);
+            if (patientInDb != null)
+            {
+                _dbEntities.Patients.Remove(patientInDb);
+                return _dbEntities.SaveChanges();
+            }
+
+            return 0;
+        }
+
+        public IEnumerable<Patient> GetAllPatients()
+        {
+            return _dbEntities.sp_PatientGetAll();
+        }
+
+        public Patient GetPatientByPhoneNo(string phoneNo)
+        {
+            return _dbEntities.sp_PatientGetByPhoneNo(phoneNo);
+        }
+
+    }
+}
